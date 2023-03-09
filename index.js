d94afdfeb6250a071e248079259253f5fa1af4a1
Syed Abdulrehman Ali Shah <57559087+Abyshah@users.noreply.github.com>
2023-03-09 13:33:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,3 @@ server.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
-// Wait for 10 seconds and then close the server
-setTimeout(() => {
-  server.close();
-  console.log('Server closed');
-}, 10000);
