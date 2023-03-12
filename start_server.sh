5a6e2e99d4ce418459fdbd0f1d043c3a38a22518
Syed Abdulrehman Ali Shah <57559087+Abyshah@users.noreply.github.com>
2023-03-12 16:00:25 +0500

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,19 @@
 npm run build > /dev/null 2>&1
+
+cat << EOF >> /etc/systemd/system/myserver.service
+Description=index.js 
+Documentation=https://example.com
+After=network.target
+[Service]
+Environment=NODE_PORT=3000
+Type=simple
+User=root
+ExecStart=/usr/bin/node /opt/codedeploy-agent/deployment-root/22c65cda-1abb-461f-87c9-24a5e5974409/d-RDPXHCRN9/deployment-archive/index.js
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable myserver.service
+sudo systemctl start myserver.service
